b04ec8b1664edbb7eb2b78feeabbb9fff18ba910
Niush Sitaula <niushsitaula@gmail.com>
2018-08-25 17:46:24 +0545

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -63,7 +63,7 @@
         <tr>
         <td>{{ $d['title'] }}</td>
         <td>{!! nl2br(e($d['note'])) !!}</td>
-        <td>{{ $d['created_at'] }}</td>
+        <td>-</td>
         <td>
             <a href="edit/{{ $d['id'] }}" class="btn success waves-effect" onclick="return confirm('Are You Sure You Want to Edit ?\n\n{{ $d['title'] }}');">Edit</a>
         </td>
