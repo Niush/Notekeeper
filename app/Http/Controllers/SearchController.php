617447d449c472c264419ae9d3f68080a5725f5d
Niush Sitaula <niushsitaula@gmail.com>
2018-08-25 17:31:57 +0545

    Update SearchController.php

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,7 +11,7 @@ class SearchController extends Controller
 {
     public function search($query, Note $note){
         if(DB::table('notes')->where('user_id',Auth::user()->id)){
-            $note_fetch = return DB::table('notes')->where('user_id',Auth::user()->id)->where('deleted',0)
+            $note_fetch = DB::table('notes')->where('user_id',Auth::user()->id)->where('deleted',0)
             ->where(function($continue) use ($query) {
                 $continue->where('title','LIKE','%'.$query.'%')
                 ->orWhere('note','LIKE','%'.$query.'%');
