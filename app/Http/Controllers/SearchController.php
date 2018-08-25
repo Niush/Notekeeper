46881674da8338f6399426dfd1fad9f6875e1c19
Niush Sitaula <niushsitaula@gmail.com>
2018-08-25 16:56:24 +0545

    Fix for "WHERE" Not Found

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,10 +9,14 @@ use Auth;
 class SearchController extends Controller
 {
     public function search($query, Note $note){
-        if($note->find(Auth::user()->id)->where('deleted',0)->where('user_id',Auth::user()->id)){
-            $note_fetch = $note->find(Auth::user()->id)->where('deleted',0)->where('user_id',Auth::user()->id)
-            ->where('title','LIKE','%'.$query.'%')
-            ->orWhere('note','LIKE','%'.$query.'%')
+        if($note->find(Auth::user()->id)){
+            $note_fetch = $note->find(Auth::user()->id)
+            ->where('user_id',Auth::user()->id)
+            ->where('deleted',0)
+            ->where(function($continue) use ($query) {
+                $continue->where('title','LIKE','%'.$query.'%')
+                ->orWhere('note','LIKE','%'.$query.'%');
+            })
             ->get();
         }else{
             $note_fetch = [];
